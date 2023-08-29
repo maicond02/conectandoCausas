34fc859b947c43b9a06fdb490c0c1007f5fda2b4
Maicon Alves <AD648ZZ@mmm.com>
2023-08-29 13:43:09 -0300

    config nuxt, and add primevue plugins

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,48 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  typescript: {
-    strict: true
-  }
+    ssr: false,
+    nitro: {
+      prerender: {
+        crawlLinks: true
+      }
+    },
+    
+    components: true,
+    app: {
+      head: {
+
+        link: [
+          { rel: "stylesheet", type: "text/css", href: "../primevue/resources/themes/saga-blue/theme.css" },
+          { rel: "stylesheet", type: "text/css", href: "../primevue/resources/primevue.css" },
+          { rel: "stylesheet", type: "text/css", href: "../primeicons/primeicons.css" },
+          { rel: "stylesheet", type: "text/css", href: "../primeflex/primeflex.css" },
+          { rel: "stylesheet", type: "text/css", href: "../assets/default.css" },
+          { rel: "stylesheet", type: "text/css", href: "../assets/icons/remixicon.cs" }
+        ],
+      },
+      buildAssetsDir: '/src/'
+    },
+    css: [
+        'primevue/resources/themes/saga-blue/theme.css',
+        'primevue/resources/primevue.css',
+        'primeicons/primeicons.css',
+        'primeflex/primeflex.css',
+    ],
+    modules: [
+        [
+          '@pinia/nuxt',
+          {
+            autoImports: [
+              'defineStore',
+              ['defineStore', 'definePiniaStore'],
+            ],
+          },
+        ],
+      ],
+
+      typescript: {
+        strict: true,
+      },
+      
 })
