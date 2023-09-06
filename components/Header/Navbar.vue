7608a2718197c0be471e21b9efaeec350447c710
Maicon Alves <AD648ZZ@mmm.com>
2023-09-06 14:06:30 -0300

    updated nuxt.config and redesing the navbar

--- components/Header/Navbar.vue
+++ components/Header/Navbar.vue
@@ -1,17 +1,50 @@
 <template>
     <div>
-        <Menubar :model="items" />
+        <Menubar :model="items" >
+            <template #end>
+                <div>
+                    <Dropdown v-model="selectedLanguage" :options="languages" optionLabel="language" placeholder="Select a City" class="" />
+                </div>
+            </template>
+        </Menubar>
     </div>
 </template>
 <style scoped>
     :deep(.p-component){
         height: 60px;
+        background-color: transparent;
+        border: none;
+    }
+    :deep(.p-menubar-root-list){
+        background-color: transparent;
+        border: none;
+    }
+    :deep(.p-menubar){
+        border-bottom: 2px solid rgb(75, 75, 75);
+    }
+    :deep(.p-dropdown-label){
+        padding: 0;
+        padding-left: 10px;
+    }
+
+    :deep(.p-dropdown){
+        height: 30px;
+        display: flex;
+        align-items: center;
     }
 </style>
-<script>
+<script lang="ts">
 export default {
     data() {
         return {
+            currentTheme: 'md-dark-indigo',
+            alternateTheme: 'md-light-indigo',
+            selectedLanguage:'',
+            languages:[
+                { language: 'English', code: 'ENG' },
+                { language: 'Português', code: 'PT' },
+                { language: 'Espanhol', code: 'ES' },
+            ],
             items: [
                 {
                     label: 'Home',
@@ -35,6 +68,6 @@ export default {
                 },
             ]
         };
-    }
+    },
 };
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,8 +8,19 @@ export default defineNuxtConfig({
       }
     },
     
+
     components: true,
     app: {
+      head:{
+        link: [
+          { rel: "stylesheet", type: "text/css", href: "../primevue/resources/themes/saga-blue/theme.css" },
+          { rel: "stylesheet", type: "text/css", href: "../primevue/resources/primevue.css" },
+          { rel: "stylesheet", type: "text/css", href: "../primeicons/primeicons.css" },
+          { rel: "stylesheet", type: "text/css", href: "../primeflex/primeflex.css" },
+          { rel: "stylesheet", type: "text/css", href: "../assets/default.css" },
+          { rel: "stylesheet", type: "text/css", href: "../assets/icons/remixicon.cs" }
+        ],
+      },
       buildAssetsDir: '/src/'
     },
     css: [
