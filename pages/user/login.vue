9c4dc53b4fba6379a5ec05084574a06b278b9999
Leonardo Marcondeli <leonardo.marcondeli@gmail.com>
2023-10-03 20:30:15 -0300

    UpdateLoginAndRegister

--- pages/user/login.vue
+++ pages/user/login.vue
@@ -1,9 +1,42 @@
-<template>
+<template #content>
     <div>
-        login
+
+            <Card> 
+                <template #content>
+                    <div class="grid">
+                        <div class="col-6">
+                            <div class="flex align-items-start justify-content-start">
+                                <h3>alo</h3>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <h3 class="flex flex-row-reverse mr-8">Login</h3>
+                            <div class="flex-column flex align-items-end justify-content-end">
+                                
+                                <InputText placeholder="Nome" class="" type="text" />
+                                <br>
+                                <InputText placeholder="Apelido" class="" type="text" />
+                                <br>
+                                <InputText placeholder="E-mail" class="" type="text" />
+                                <br>
+                                <InputText placeholder="Password" class="" type="password"/>
+                            </div>
+                        </div>
+                    </div>
+                </template>
+
+            </Card>
+
     </div>
 </template>
 
 <script lang="ts">
 
 </script>
+
+<style scoped>
+.teste1{
+    margin-right: -0.5rem;
+}
+
+</style>
--- pages/user/register.vue
+++ pages/user/register.vue
@@ -1,19 +1,22 @@
 <template #content>
-    <div class="centered-card">
-        <img class="backgroundimage" src="../../public/registerimage.png" alt="">
-        <Card class="fundo">
+    <div class="grid">
+      <div class="col-6">
+        <img class="w-12" src="../../public/registerimage.png" alt="">
+      </div>
+        
+        <Card class="col-6 cardinv testando flex justify-content-end">
             <template #content>
                 <div class="conteudo flex flex-column">
                     <h3 class="titulo">CONNECTING CAUSES</h3>
                     <h3 class="titulo2">REGISTER</h3>
                     <p class="letrasmenu">Name</p>
-                    <InputText type="text" v-model="value" />
+                    <InputText type="text"/>
                     <p class="letrasmenu">Surname</p>
-                    <InputText type="text" v-model="value" />
+                    <InputText type="text"/>
                     <p class="letrasmenu">E-mail</p>
-                    <InputText type="text" v-model="value" />
+                    <InputText type="text"/>
                     <p class="letrasmenu">Password</p>
-                    <Password class="passwordbox" v-model="value" :feedback="false" />
+                    <Password class="passwordbox" :feedback="false" />
                 </div>
             </template>
         </Card>
@@ -23,56 +26,15 @@
     </template>
 </template>
 
+<style scoped>
 
+.cardinv{
+  background-color: transparent;
+}
 
+</style>
 
 
 <script lang="ts">
 
-
-
 </script>
-
-<style scoped>
-.fundo{
-  /*  min-height: 30%;
-    min-width: 15%; */
-    min-height: 45%;
-    min-width: 15%;
-}
-.letrasmenu{
-    font-size: larger;
-}
-.passwordbox{
-  min-width: 100%;
-  display: flex;
-  justify-content: start;
-  align-items: start;
-}
-.centered-card {
-  display: flex;
-  justify-content: center;
-  align-items: center;
- /* margin-top: 20vh; */
-}
-.titulo{
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.titulo2{
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: x-large;
-}
-.backgroundimage{
-    z-index: 0;
-    position: relative;
-    max-height: 1500px;
-    left: 1px;
-    margin-top: -10px;
-    display: flex;
-    left: -10%;
-}
-</style>
