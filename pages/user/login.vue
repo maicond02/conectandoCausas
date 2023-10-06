ceb9ad82f40d819a6c3005b2c1a5d79dab982169
LeonardoMarcondeli <leonardo.marcondeli@gmail.com>
2023-10-05 22:34:59 -0300

    UpdatePageLogin

--- pages/user/login.vue
+++ pages/user/login.vue
@@ -1,32 +1,32 @@
 <template #content>
     <div>
-
-            <Card> 
-                <template #content>
-                    <div class="grid">
-                        <div class="col-6">
-                            <div class="flex align-items-start justify-content-start">
-                                <h3>alo</h3>
-                            </div>
-                        </div>
-                        <div class="col-6">
-                            <h3 class="flex flex-row-reverse mr-8">Login</h3>
-                            <div class="flex-column flex align-items-end justify-content-end">
-                                
-                                <InputText placeholder="Nome" class="" type="text" />
-                                <br>
-                                <InputText placeholder="Apelido" class="" type="text" />
-                                <br>
-                                <InputText placeholder="E-mail" class="" type="text" />
-                                <br>
-                                <InputText placeholder="Password" class="" type="password"/>
-                            </div>
+         <Card class="card1"> 
+            <template #content>
+                <div class="grid">
+                    <div class="col-6">
+                         <div class="flex align-items-start justify-content-start">
+                             <img class="w-12 img" src="../../public/loginimage.png" alt="">
                         </div>
                     </div>
-                </template>
-
-            </Card>
-
+                     <div class="col-6">
+                              <div class="flex-column flex align-items-end justify-content-end mx-8 mt-8">
+                                  <Card class="card2">
+                                      <template #content>
+                                          <h3 class="flex flex-row-reverse mr-8">Login</h3>
+                                          <InputText placeholder="Nome" class="flex border-round" type="text" />
+                                          <br>
+                                          <InputText placeholder="Apelido" class="flex border-round" type="text" />
+                                          <br>
+                                          <InputText placeholder="E-mail" class="flex border-round" type="text" />
+                                          <br>
+                                          <InputText placeholder="Password" class="flex border-round" type="password"/>
+                                      </template>
+                                   </Card>
+                               </div>
+                       </div>
+                   </div>
+            </template>
+         </Card>
     </div>
 </template>
 
@@ -37,6 +37,13 @@
 <style scoped>
 .teste1{
     margin-right: -0.5rem;
+}
+.card2{
+  width: 16rem;
+  height: 100%;
+}
+.card1{
+    background-color: transparent;
 }
 
 </style>
