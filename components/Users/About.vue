accee6ac1b029d60deeeb799de04fec3d6430315
Leonardo Marcondeli <leonardo.marcondeli@gmail.com>
2023-10-23 21:19:54 -0300

    UpdateAboutContent

--- components/Users/About.vue
+++ components/Users/About.vue
@@ -4,14 +4,34 @@
             <template #content>
                 <div class="grid">
                     <div class="cardprimario flex justify-content-center col-12">
-                        <h3>Welcome to "Conectando Causas"!</h3>
+                        <h3>Bem-vindo ao "Conectando Causas"!</h3>
                     </div>
-                    <div class="texto flex justify-content-center col-12">
-                        <p style="text-align: center;" class="">We are a non-governmental organization committed to uniting individuals passionate about noble causes and collaborating to make the world a better place. Our purpose is to foster connections to generate a positive impact.</p>
+                    <div class="texto flex justify-content-center col-12 mt-4">
+                        <p style="text-align: center; font-size: large;" class="">Somos uma organização não governamental comprometida em unir indivíduos apaixonados por causas nobres e colaborar para tornar o mundo um lugar melhor. Nosso objetivo é promover conexões para gerar um impacto positivo.</p>
                     </div>
-                    <div class="flex justify-content-center grid">
+                    <Divider/>
+                    <div class="flex justify-content-start grid">
                         <div class="col-6">
                             <img class="w-12" src="../../Aboutimag.png" alt="">
+                        </div>
+                        <div class="col-6">
+                            <h3 style="text-align: center;" >Nossa Missão</h3>
+                            <p style="text-align: start;">Nossa missão é simples, mas poderosa: apoiar ONGs em sua busca por um mundo mais justo, igualitário e sustentável. Acreditamos que, juntos, podemos causar um impacto real e duradouro. Oferecemos às ONGs as ferramentas e recursos necessários para amplificar sua voz, arrecadar fundos de maneira eficiente e alcançar um público maior.</p>
+                                <Divider />
+                            <h3 style="text-align: center;" >O que Oferecemos</h3>
+                            <p style="text-align: start;">
+                                <ul>
+                                    <li>Uma plataforma fácil de usar que conecta ONGs a voluntários, doadores e recursos valiosos.</li>
+                                    <li>Ferramentas de arrecadação de fundos e promoção de campanhas.</li>
+                                    <li>Recursos de treinamento e capacitação para fortalecer organizações e causas.</li>
+                                    <li>Um espaço para compartilhar histórias inspiradoras e destacar os sucessos das ONGs.</li>
+                                </ul>
+                            </p>
+                                <Divider />
+                            <h3 style="text-align: center;" >Como Você Pode Fazer a Diferença</h3>
+                            <p style="text-align: start;">Você também pode fazer parte dessa jornada transformadora. Explore nossa plataforma, descubra ONGs incríveis e causas que ressoam com você e comece a fazer a diferença hoje. Seja voluntário, faça uma doação, compartilhe suas habilidades e experiência ou simplesmente ajude a espalhar a palavra.</p>
+                            <p style="text-align: start;">Juntos, podemos criar um mundo mais solidário e compassivo, onde todas as ONGs tenham as ferramentas e o apoio necessários para realizar suas missões com sucesso.</p>
+                            <p style="text-align: start;">Junte-se a nós e faça parte desta comunidade dedicada a tornar o mundo um lugar melhor. Juntos, somos a mudança que desejamos ver no mundo.</p>
                         </div>
                     </div>
                 </div>
